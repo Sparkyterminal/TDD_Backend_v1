8fc7c5006533f0ff4d9e49cd25698a56883adf26
gcnaveen <naveengc99@gmail.com>
2025-09-25 16:42:52 +0530

    workshop creation

--- routes/Workshops/index.js
+++ routes/Workshops/index.js
@@ -4,10 +4,11 @@ const workshopController = require('../../controlers/Workshops');
 
 router.post('/', workshopController.createWorkshop);
 router.get('/', workshopController.getWorkshops);
+router.post('/book', workshopController.bookWorkshop);
+router.get('/check-status', workshopController.getStatusOfPayment);
 router.get('/:id', workshopController.getWorkshop);
 router.put('/:id', workshopController.updateWorkshop);
 router.delete('/:id', workshopController.cancelWorkshop);
-router.post('/book', workshopController.bookWorkshop);
-router.get('/check-status', workshopController.getStatusOfPayment);
+
 
 module.exports = router;
