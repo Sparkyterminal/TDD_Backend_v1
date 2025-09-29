e78877d47af35e39f8f475eab6372e88d9765d3f
gcnaveen <naveengc99@gmail.com>
2025-09-29 23:29:52 +0530

    membership plan

--- routes/MembershipPlans/index.js
+++ routes/MembershipPlans/index.js
@@ -4,11 +4,11 @@ const plansController = require('../../controlers/MembershipPlans');
 
 router.post('/', plansController.createPlan);
 router.get('/', plansController.getPlans);
+router.post('/booking', plansController.createBooking);
+router.get('/check-status', plansController.checkMembershipStatus);
 router.get('/:id', plansController.getPlanById);
 router.put('/:id', plansController.updatePlan);
 router.delete('/:id', plansController.deletePlan);
-router.post('/booking', plansController.createBooking);
-router.get('/check-status', plansController.checkMembershipStatus);
 
 module.exports = router;
 
