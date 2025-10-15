168559710a2812c4cf1b215dab811c7ce7fc14ea
gcnaveen <naveengc99@gmail.com>
2025-10-15 22:16:55 +0530

    gate way changes

--- controlers/MembershipPlans/index.js
+++ controlers/MembershipPlans/index.js
@@ -835,6 +835,8 @@ exports.checkMembershipStatus = async (req, res) => {
 
             // Decrement capacity of the booked batch
             const plan = await MembershipPlan.findById(booking.plan);
+            console.log('plan', plan);
+            console.log('booking.batchId', booking.batchId);
             if (plan && plan.batches && booking.batchId) {
                 const batch = plan.batches.id(booking.batchId);
                 if (batch && batch.capacity !== undefined && batch.capacity > 0) {
