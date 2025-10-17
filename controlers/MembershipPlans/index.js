a7f75c0cbc2f24f6048dddd94753e54572236ead
gcnaveen <naveengc99@gmail.com>
2025-10-17 11:55:14 +0530

    gate way changes

--- controlers/MembershipPlans/index.js
+++ controlers/MembershipPlans/index.js
@@ -7,8 +7,8 @@ const User = require('../../modals/Users');
 const bcrypt = require('bcryptjs');
 const {StandardCheckoutClient, Env, StandardCheckoutPayRequest} = require('pg-sdk-node')
 const jwt = require('jsonwebtoken');
-const clientId = process.env.CLIENT_ID
-const clientSecret = process.env.CLIENT_SECRET
+const clientId = 'SU2510151630541019332818'
+const clientSecret = '1d277db1-018a-4a96-9300-24018ee0d6eb'
 const env = Env.PRODUCTION
 // const { isValidObjectId, Types } = require('mongoose');
 
